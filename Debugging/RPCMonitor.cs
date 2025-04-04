28127a0f985c90f1d3db6af5fb200dc03b08b741
KanteV2 <tn.us3r@gmail.com>
2025-04-03 23:20:23 -0400

    'Kitty'

---
+++ Debugging/RPCMonitor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Kitty.Debugging
+{
+    [HarmonyPatch(typeof(NetworkView), "SendRPC")]
+    internal class RPCMonitor
+    {
+        [HarmonyPrefix] 
+        static bool Prefix(string method, RpcTarget target, params object[] parameters)
+        {
+            KittyMonitor.Monitor($"An RPC has been called! \n METHOD: {method} \n TARGET {target.ToString()} \n PARAMS: {parameters.ToString()}", KittyMonitor.LogLevel.Info);
+            return true;
+        }
+    }
+}
